4d52fdcc3972eff7d3a4229eb213ab1415ab8bbf
madhua-cmr <madhumithaacmr@gmail.com>
2025-07-29 10:07:53 +0530

    font poppins added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import{Poppins} from "next/font/google"
 import "./globals.css";
 import { Toaster } from "sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+const poppins=Poppins({subsets:['latin'],  weight: ['400', '700']})
 export const metadata: Metadata = {
   title: "BitPrep",
   description: "Ai powered platform to take mock interviews",
@@ -26,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" >
       <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-one text-two antialiased`} 
+        className={`${poppins.className} bg-one text-two antialiased`} 
       ><div className=" flex-col-cen ">
         {children}
         <Toaster/>
